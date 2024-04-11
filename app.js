957329a4907d97a26f718204d7f6fcbd06407377
ajithasamuel38 <ajithasamuel38@gamil.com>
2024-04-11 13:37:41 +0530

    userproductmodel

--- app.js
+++ app.js
@@ -15,18 +15,45 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const Product = require('./models/product');
+
+const User = require('./models/user');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) =>{
+     User.findByPk(1)
+     .then((result) =>{
+           req.user = result;
+           next();
+        }).catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then((result)=>{
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product)
+
+sequelize//.sync( {force: true})
+ .sync().then((result) =>{
+    return User.findByPk(1);
+ })
+.then((user)=>{
+    if(!user){
+        return User.create({ name: 'Ajitha', email:'ajitha@gmail.com'});
+    }
+    return user;
+})
+.then((result)=>{
+    
     console.log(result);
     app.listen(4000);
-}).catch((err)=>{
+})
+.catch((err)=>{
     console.log(err)
 });
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title:title,
     price: price,
     imageUrl: imageUrl,
     description: description
-  }).then(result =>{
+  })
+  .then(result =>{
     console.log(result);
     res.redirect('/admin/products');
   }).catch(err =>{
@@ -40,9 +41,10 @@ exports.getEditProduct = (req, res, next) => {
   const prodId = req.params.productID;
 
   console.log(prodId);
-
-  Product.findByPk(prodId).then(product =>{
+    req.user.getProducts({where : {id: prodId} }).then(products =>{
+      const product = products[0];
     if(!product){
+
       return res.redirect('/');
     }
     res.render('admin/edit-product', {
@@ -99,7 +101,7 @@ exports.postDeleteProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
   const prodid = req.body.productId;
 
-  Product.findAll().then((products) => {
+  req.user.getProducts().then((products) => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
